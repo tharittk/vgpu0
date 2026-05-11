989fb3a75ce97817beeed02757b25fea2ee562ed
tharittk <tharit.tangkij@gmail.com>
2026-05-11 22:07:25 +0700

    memory region pci dev

--- pci/vgpu0-dev.c
+++ pci/vgpu0-dev.c
@@ -4,23 +4,50 @@
 #include "hw/core/qdev-properties.h"
 #include "qemu/module.h"
 #include "qom/object.h"
+#include "system/system.h"
 
 #define TYPE_PCI_VGPU0_DEVICE "vgpu0"
 #define PCI_VENDOR_ID_VGPU0 0xdead
 #define PCI_DEVICE_ID_VGPU0 0xbeef
 
+#define PCI_BASE_ADDRESS_IO 0x1
+
 
 typedef struct vgpu0State vgpu0State;
 struct vgpu0State {
 	PCIDevice pdev;
+    MemoryRegion mmio_bar0;
+    uint8_t bar0[64];
 };
 
 DECLARE_INSTANCE_CHECKER(vgpu0State, VGPU0, TYPE_PCI_VGPU0_DEVICE)
 
+static uint64_t vgpu0_io_read (void *opaque, hwaddr addr, unsigned size)
+{
+    printf("vgpu0 - IO read !\n");
+    return 0;
+}
+
+static void vgpu0_io_write (void *opaque, hwaddr addr, uint64_t val, unsigned size){
+    printf("vgpu0 - IO write!\n");
+}
+
+
+static const MemoryRegionOps vgpu0_io_ops = {
+    .read = vgpu0_io_read,
+    .write = vgpu0_io_write,
+};
+
 static void vgpu0_realize(PCIDevice *pdev, Error **errp)
 {
-	//vgpu0State *vgpu0 = VGPU0(pdev);
-	printf("VGPU0 - Realized !\n");
+	vgpu0State *s = VGPU0(pdev);
+    //DeviceState *d = DEVICE(pdev);
+
+    memory_region_init_io(&s->mmio_bar0, OBJECT(s), &vgpu0_io_ops, s, "vgpu0-io", 0x40);
+
+    pci_register_bar(pdev, 0, PCI_BASE_ADDRESS_SPACE_MEMORY, &s->mmio_bar0);
+
+	printf("vgpu0 - realized with iosize: 0x%x\n", (unsigned int)s->mmio_bar0.size);
 }
 
 static void vgpu0_uninit(PCIDevice *pdev)
--- run_qemu.sh
+++ run_qemu.sh
@@ -2,7 +2,7 @@
 
 ./env/qemu-11.0.0/build/qemu-system-aarch64 \
     -M virt \
-    -cpu host -enable-kvm \
+    -cpu max \
     -kernel env/linux-7.0.3/arch/arm64/boot/Image \
     -initrd env/rootfs.cpio.gz \
     -append "console=ttyAMA0 root=/dev/ram rdinit=/init" \
