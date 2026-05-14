2faeb9a4c1f7f850b654a94fc71d9a3b7c1bff43
tharittk <tharit.tangkij@gmail.com>
2026-05-14 17:26:42 +0700

    some more

--- wayland/wayland0.c
+++ wayland/wayland0.c
@@ -1,26 +1,111 @@
 // Wayland from scratch for my own educational purpose
 // https://wayland-book.com/protocol-design/wire-protocol.html
 // Message: object_id [32], size msg [16 upper], event opcode [16 lower], args[..]
-//
+
+#include <assert.h>
+#include <errno.h>
+#include<fcntl.h>
+#include<stddef.h>
+#include<stdint.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/mman.h>
+#include<sys/socket.h>
+#include<sys/time.h>
+#include<sys/un.h>
+#include<unistd.h>
+
+// clearing the last two bits = round-down to nearest 4
+#define roundup_4(x) ((x+3)a & -4)
+
+#define cstring_len(s) (sizeof(s) - 1)
+
+typedef struct state_t state_t;
+struct state_t {
+	uint32_t wl_registry;
+	uint32_t wl_shm;
+	uint32_t wl_shm_pool;
+	uint32_t wl_buffer;
+	uint32_t wl_compositor;
+	uint32_t wl_surface;
+	uint32_t xdg_wm_base;
+	uint32_t xdg_surface;
+	uint32_t xdg_toplevel;
+	uint32_t stride;
+	uint32_t w;
+	uint32_t h;
+	uint32_t shm_pool_size;
+	int shm_fd;
+	uint8_t *shm_pool_data;
+};
 
 static int wayland_display_connect ()
 {
 	/* use XDG environment variable and UNIX socket */
+	char* xdg_runtime_dir = getenv("XDG_RUNTIME_DIR");
+	if (xdg_runtime_dir == NULL)
+		return EINVAL;
+
+	uint64_t xdg_runtime_dir_len = strlen(xdg_run_time_dir);
+
+	struct socket_un addr = {.sun_family = AF_UNIX};
+	uint64_t socket_path_len = 0;
+
+	memcpy(addr.sun_path, xdg_runtime_dir, xdg_runtime_dir_len);
+	socket_path_len += xdg_runtime_dir_len;
+			
+	// concatenate with wayland
 
 	/* return a file descriptor */
 	return 0;
 }
 
 /* utilities for writing u16, u32, string to buffer */
-static void buf_write_u32 (char *buf, uint64_t *buf_size, uint64_t *buf_cap, uint32_t val);
-static void buf_write_u16 (char *buf, uint64_t *buf_size, uint64_t *buf_cap, uint16_t val);
-static void buf_write_string(char *buf, uint64_t *buf_size, uint64_t *buf_cap, char *src, uint32_t src_len);
+static void buf_write_u32 (char *buf, uint64_t *buf_size, uint64_t *buf_cap, uint32_t val)
+{
+	assert (*buf_size + 4 <= *buf_cap);
+
+	*(uint32_t *) (buf + *buf_size) = val;
+	*buf_size += 4;
+}
+static void buf_write_u16 (char *buf, uint64_t *buf_size, uint64_t *buf_cap, uint16_t val)
+{
+	assert (*buf_size + 2 <= *buf_cap);
+	*(uint16_t *) (buf + *buf_size) = val;
+	*buf_size += 2;
+}
+static void buf_write_string(char *buf, uint64_t *buf_size, uint64_t *buf_cap, char *src, uint32_t src_len)
+{
+	assert(*buf_size + src_len <= *buf_cap);
+	memcpy(buf + *buf_size, src, src_len);
+	// could be not 4-byte align. Need round up
+	*buf_size += src_len;
+}
 
 /* utilities for read u16, u32, string to buffer */
 // note: we are going to advance *buf as well
-static uint32_t buf_read_u32(char **buf, uint64_t *buf_size);
-static uint16_t buf_read_u32(char **buf, uint64_t *buf_size);
-static uint16_t buf_read_nchar(char **buf, uint64_t *buf_size, char *dst, uint64_t n);
+static uint32_t buf_read_u32(char **buf, uint64_t *buf_size)
+{
+	assert(*buf_size >= 4);
+	uint32_t val = *(uint32_t*)(*buf);
+	*(buf) += 4;
+	*(buf_size) -= 4;
+}
+static uint16_t buf_read_u16(char **buf, uint64_t *buf_size)
+{
+	assert(*buf_size >= 2);
+	uint16_t val = *(uint16_t*)(*buf);
+	*(buf) += 2;
+	*(buf_size) -= 2;
+}
+static uint16_t buf_read_nchar(char **buf, uint64_t *buf_size, char *dst, uint64_t n)
+{
+	assert(*buf_size >= n);
+	memcpy(dest, *buf, n);
+	*buf += n;
+	*buf_size -= n;
+}
 
 /* get registry for the new object id, return the new object id */
 static uint32_t wayland_wl_display_get_registry(int fd)
